2ed2213f12d04264c5dca6403e606ce4008a1dc9
Alexey Khokhlov <alexeyxo@gmail.com>
2015-04-27 21:24:22 +0300

    brew formula #56

---
+++ protobuf-swift.rb
@@ -0,0 +1,32 @@
+class ProtobufSwift < Formula
+  homepage "https://github.com/alexeyxo/protobuf-swift"
+  url "https://github.com/alexeyxo/protobuf-swift/archive/1.6.tar.gz"
+  sha256 "1581301212fd8c3aa735e3a9a42444e4f5cb86ac9caf5f477a917f4dfe0eb2e1"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "protobuf"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+     testdata = <<-EOS.undent
+       enum Flavor{
+         CHOCOLATE = 1;
+          VANILLA = 2;
+        }
+        message IceCreamCone {
+          optional int32 scoops = 1;
+          optional Flavor flavor = 2;
+        }
+    EOS
+    (testpath/"test.proto").write(testdata)
+    system "protoc", "test.proto", "--swift_out=." 
+  end
+end
