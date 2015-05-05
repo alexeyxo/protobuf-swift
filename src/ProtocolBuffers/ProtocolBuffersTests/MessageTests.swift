1f16e94b95e8fb2a1df8edf54f60e37e6eeb3f07
Alexey Khokhlov <alexeyxo@gmail.com>
2015-05-05 10:32:47 +0300

    Add tests for #61

--- src/ProtocolBuffers/ProtocolBuffersTests/MessageTests.swift
+++ src/ProtocolBuffers/ProtocolBuffersTests/MessageTests.swift
@@ -9,6 +9,17 @@
 import Foundation
 import XCTest
 
+
+func == (lhs: RegularPoint, rhs: RegularPoint) -> Bool {
+    return lhs.latitude == rhs.latitude &&
+        lhs.longitude == rhs.longitude
+}
+
+class RegularPoint: Equatable {
+    var latitude:Float = Float(0)
+    var longitude:Float = Float(0)
+}
+
 class MessageTests: XCTestCase {
 
     override func setUp() {
@@ -127,4 +138,42 @@ class MessageTests: XCTestCase {
         XCTAssertFalse(message.buildPartial().isInitialized(), "")
     }
     
+    
+    ///Issue #61 
+    func testProtoPointWorks() {
+        var point1 = PBProtoPoint.builder().setLatitude(1.0).setLongitude(1.0).build()
+        var point2 = PBProtoPoint.builder().setLatitude(2.0).setLongitude(2.0).build()
+        
+        XCTAssert(point1.latitude == 1.0, "")
+        XCTAssert(point2.latitude == 2.0, "")
+        
+        // Succeeds, calls the == function from ProtoPoint.pb.swift as expected
+        XCTAssert(!(point1 == point2), "")
+    }
+    
+    func testProtoPointShouldWork() {
+        var point1 = PBProtoPoint.builder().setLatitude(1.0).setLongitude(1.0).build()
+        var point2 = PBProtoPoint.builder().setLatitude(2.0).setLongitude(2.0).build()
+        
+        XCTAssert(point1.latitude == 1.0, "")
+        XCTAssert(point2.latitude == 2.0, "")
+        
+        // Fails, should call the == function from ProtoPoint.pb.swift and take the inverse just like the testRegularPoint() does. But that doesn't happen.
+        XCTAssert(point1 != point2, "")
+    }
+}
+
+class RegularPointEqualityTest: XCTestCase {
+    func testRegularPoint() {
+        var point1 = RegularPoint()
+        point1.latitude = 1.0
+        point1.longitude = 1.0
+        
+        var point2 = RegularPoint()
+        point2.latitude = 2.0
+        point2.longitude = 2.0
+        
+        // works fine, calls the == function and takes the inverse implicitly (put break point to check)
+        XCTAssert(point1 != point2, "")
+    }
 }
