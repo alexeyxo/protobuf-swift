9c087b74a139551a214d55bb730e9362b0e9778c
Alexey Khokhlov <alexeyxo@gmail.com>
2015-06-24 17:07:48 +0300

    README.md
    Add: ProtocolBuffers 3.0

--- scripts/makeDescriptor.sh
+++ scripts/makeDescriptor.sh
@@ -14,13 +14,13 @@ scripts/build.sh
 
 # compile the swift descriptors and utils into the runtime library
 protoc -I$compiler_root $compiler_root/google/protobuf/{,swift-}descriptor.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/any.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/api.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/duration.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/empty.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/field_mask.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/source_context.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/struct.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/timestamp.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/type.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
-protoc -I$compiler_root $compiler_root/google/protobuf/wrappers.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+protoc -I$compiler_root $compiler_root/google/protobuf/Utilities/*.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/api.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/duration.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/empty.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/field_mask.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/source_context.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/struct.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/timestamp.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/type.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
+# protoc -I$compiler_root $compiler_root/google/protobuf/wrappers.proto --swift_out=src/ProtocolBuffers/runtime-pb-swift/
