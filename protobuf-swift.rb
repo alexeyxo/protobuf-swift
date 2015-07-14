b0aa947086b92c7e3e9cfa90010480acfa2d33d6
Alexey Khokhlov <alexeyxo@gmail.com>
2015-07-14 12:32:47 +0300

    brew formula

--- protobuf-swift.rb
+++ protobuf-swift.rb
@@ -1,7 +1,15 @@
 class ProtobufSwift < Formula
+  desc "An implementation of Protocol Buffers in Swift."
   homepage "https://github.com/alexeyxo/protobuf-swift"
-  url "https://github.com/alexeyxo/protobuf-swift/archive/1.6.tar.gz"
-  sha256 "1581301212fd8c3aa735e3a9a42444e4f5cb86ac9caf5f477a917f4dfe0eb2e1"
+  url "https://github.com/alexeyxo/protobuf-swift/archive/2.0.tar.gz"
+  sha256 "88e45f8850923cf33cba82ef1bf15dda345b65ad803a1f273efcd1b7f24f8041"
+
+  bottle do
+    cellar :any
+    sha256 "0058335c88d681f8cb96c6aeb04a894e3828dde3fe665e20512fe1f6c005fd7c" => :yosemite
+    sha256 "84c17263f38e2dbf4cafd77bbe24de289c9f2ed9ca882ea7cf1c4bf29c9a9e1b" => :mavericks
+    sha256 "de4e40a0288f3790333c0081e26cf1140beacbfbf942cf179d0849bc077edf62" => :mountain_lion
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
